56cf8b2d12acd06451803a28fa87a4ba4a9f559e
parsaEsfandpour <parsa.esfandpoor89@gmail.com>
2024-12-12 05:24:32 -0800

    Add files via upload

---
+++ manifest.js
@@ -0,0 +1,14 @@
+{
+  "name": "TON Wallet Payment",
+  "short_name": "TON Payment",
+  "url": "https://parsaesfandpour.github.io/", 
+  "icons": [
+    {
+      "src": "https://parsaesfandpour.github.io/icon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ],
+  "theme_color": "#000000",
+  "background_color": "#ffffff"
+}
